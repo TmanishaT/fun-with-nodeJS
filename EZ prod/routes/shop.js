b4c303e0a11551659b10ddd077a8efef62c2cfab
TmanishaT <66744472+TmanishaT@users.noreply.github.com>
2021-03-30 13:28:46 -0400

    Changes to views and layouts using Handlebars template engine

--- EZ prod/routes/shop.js
+++ EZ prod/routes/shop.js
@@ -9,7 +9,8 @@ const router = express.Router();
 
 router.get('/', (req, res, next) => {
   const products = adminData.products;
-  res.render('shop', {prods: products, docTitle: 'Shop', path: '/', pageTitle: 'Shop'});
+  res.render('shop', {prods: products, docTitle: 'Shop', path: '/', pageTitle: 'Shop',
+   hasProds:products.length >0 , activeShop : true, productCss: true});
 });
 
 module.exports = router;
